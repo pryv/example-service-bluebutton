50d89a28f5e98fdc78f08ac3c91d71854cf48a60
Perki <pm@perki.com>
2016-11-07 15:19:52 +0100

    added login form

--- src/routes/login.js
+++ src/routes/login.js
@@ -5,14 +5,11 @@ var router = express.Router(),
 router.post('/', function (req, res, next) {
 
   var body = req.body;
-  if (! body.username || body.username.length <= 5) {
+  if (! body.username || body.username.length <= 4) {
     return next('Error: invalid username');
   }
   if(! body.password || body.password.length <= 6) {
     return next('Error: invalid password');
-  }
-  if(! body.email || body.email.length <= 3) {
-    return next('Error: invalid email');
   }
 
   password.set(body);
