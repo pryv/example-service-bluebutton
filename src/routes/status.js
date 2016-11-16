7f3b0f8c89896ce7a89b6544ebb58534f4e1574c
Thiebaud <tmodoux@pryv.com>
2016-11-16 14:53:14 +0100

    Rollback watcher: package.json and status route

--- src/routes/status.js
+++ src/routes/status.js
@@ -4,26 +4,26 @@ var express = require('express'),
 var db = require('../storage/db');
 
 router.post('/', function (req, res, next) {
-  var body = req.body,
-      username = body.username;
+  res.writeHead(200, { 'Content-Type': 'application/octet-stream',
+    'Content-Type': 'text/html; charset=utf-8',
+    'Transfer-Encoding': 'chunked'
+  });
 
-  if(db.infos(username).token === body.token) {
-    res.writeHead(200, { 'Content-Type': 'application/octet-stream',
-      'Content-Type': 'text/html; charset=utf-8',
-      'Transfer-Encoding': 'chunked'
-    });
-
-    db.watchLog(username, function(change) {
-      if(change) {
-        res.write(change);
-      } else {
-        db.unwatchLog(username);
-        res.end();
-      }
-    });
-  } else {
-    res.end('Invalid credentials!');
+  function tick() {
+    var body = req.body,
+        username = body.username;
+    if(db.infos(username).token === body.token) {
+      res.write(db.log(username));
+    } else {
+      res.write('Invalid credentials!');
+    }
   }
+
+  // TODO: update only when changes
+  setTimeout(tick, 1000);
+
+  // TODO: complete with res.end, req.on close
+
 });
 
 module.exports = router;
