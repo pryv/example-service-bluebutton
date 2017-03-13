60571b576cff7541e1851c002bb4fc8c7d22c365
Kaspar Schiess <kaspar@pryv.com>
2017-03-13 16:37:13 +0100

    Import template

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:4-onbuild
-
-# app default port
-EXPOSE 5780
-
-
-ENV APP_CONFIG "/var/pryv/service-backup.config.json"
-ENV NODE_ENV production
---
+++ build/backup/release.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+source /pd_build/buildconfig
+
+target_dir="/app/bin"
+log_dir="/app/log"
+conf_dir="/app/conf"
+data_dir="/app/data"
+
+header "Install application from release.tar"
+
+run mkdir -p $target_dir
+run chown app $target_dir
+
+# Unpack the application and run npm install. 
+pushd $target_dir
+run tar -x --owner app -f \
+  /pd_build/release.tar .
+
+PYTHON=$(which python2.7) run yarn install
+
+# Perform a release build of the source code. (-> lib)
+run npm run release
+
+# Install the config file
+run mkdir -p $conf_dir && \
+  cp /pd_build/config/backup.json $conf_dir/backup.json
+
+# Create the log
+run mkdir -p $log_dir && \
+  touch $log_dir/backup.log && chown -R app:app $log_dir
+
+# Create the data space (attachments/previews)
+run mkdir -p $data_dir/attachments && mkdir -p $data_dir/previews && \
+  chown -R app:app $data_dir
+
+# Install the script that runs the api service
+run mkdir /etc/service/backup
+run cp /pd_build/runit/backup /etc/service/core/run
