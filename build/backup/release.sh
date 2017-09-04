c8fe9e16e3b94e69f59997dc7b3c4297cd4efcf7
Thiébaud Modoux <tmodoux@pryv.com>
2017-09-04 17:55:27 +0200

    Update docker files

--- build/backup/release.sh
+++ build/backup/release.sh
@@ -12,9 +12,12 @@ header "Install application from release.tar"
 run mkdir -p $target_dir
 run chown app $target_dir
 
+# This will avoid getting DOSed by unicode.org because of the unicode npm package.
+minimal_apt_get_install unicode-data
+
 # Unpack the application and run npm install.
 pushd $target_dir
-run tar -x --owner app -f \
+run run tar -x --owner app -f \
   /pd_build/release.tar .
 
 PYTHON=$(which python2.7) run yarn install
@@ -38,9 +41,11 @@ run mkdir -p $log_dir && \
   touch $log_dir/backup.log && chown -R app:app $log_dir
 
 # Create the data space (attachments/previews)
-run mkdir -p $data_dir/attachments && mkdir -p $data_dir/previews && \
-  chown -R app:app $data_dir
+run mkdir -p $data_dir/attachments && chown -R app:app $data_dir
 
 # Install the script that runs the api service
-run mkdir /etc/service/backup && \
-  cp /pd_build/runit/backup /etc/service/backup/run
+run mkdir /etc/service/backup
+run cp /pd_build/runit/backup /etc/service/backup/run
+
+# Have CRON run in this container
+run rm /etc/service/cron/down
